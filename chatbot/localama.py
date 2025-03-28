62e11b2ba692252a4814711c748cc57ec295ad20
charles <yinchao.guo@hpe.com>
2025-03-28 10:26:40 -0700

    add secrets

--- chatbot/localama.py
+++ chatbot/localama.py
@@ -3,13 +3,12 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms import Ollama
 import streamlit as st
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+OPENAI_API_KEY=st.secrets["OPENAI_API_KEY"]
+## Langmith tracking
+LANGCHAIN_TRACING_V2="true"
+LANGCHAIN_API_KEY=st.secrets["LANGCHAIN_API_KEY"]
 
 ## Prompt Template
 
